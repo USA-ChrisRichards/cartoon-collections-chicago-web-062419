80fa8578d638b33b9914af0cc9a91c7801bf60e2
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 20:12:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 
 def long_planeteer_calls(calls)
   calls.map! {|call| call.capitalize + "!"}
-  calls.none? {|word| word.length <= 4}
+  calls.none? {|word| word.length <= 3}
   calls.any? {|word| word.length > 4}
   end
 end
